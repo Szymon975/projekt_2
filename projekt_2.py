d51f226b28f64e4154118c591ecc5d8c5565a955
Szymon975 <61831227+Szymon975@users.noreply.github.com>
2020-06-06 03:04:53 +0200

    liczy tor przy oddzialywaniu tylko od slonca

--- projekt_2.py
+++ projekt_2.py
@@ -1,98 +1,88 @@
 import numpy as np
 import scipy
 import math as m
-import turtle
+import numpy as np
+from scipy import integrate
 import random
-import time
+import multiprocessing
 
-wn=turtle.Screen()
-wn.setup(width=1920,height=1200)
-wn.bgcolor("black")
-wn.title("Apocalypse Simulation")
+number_of_asteroids = 3
 
-m_sun=5
-m_moon=1
-m_earth=2
-m_asteroid=0.1
+Y = np.zeros(shape=(number_of_asteroids,4))
 
-moon = turtle.Turtle()
+asteroid_sun_distance=5
+mars_orbiting_speed=24
 
-radius=90
-sun_distance=400
-moon.shape("circle")
-moon.color("gray")
-moon.resizemode()
-moon.shapesize(stretch_wid=1, stretch_len=1)
-moon.penup()
-moon.speed(0)
-moon.goto(-radius-sun_distance,0)
+#24 km/s
+print(Y)
 
-earth = turtle.Turtle()
+def vec_function(x,y):
+	return np.sqrt(np.power(x,2)-np.power(y,2))
 
-earth.shape("circle")
-earth.color("blue")
-earth.resizemode()
-earth.shapesize(stretch_wid=2.3, stretch_len=2.3)
-earth.penup()
-earth.speed(0)
-earth.goto(-sun_distance,0)
+def cylindrical_to_cartesian_transformation(r,theta,Z):
+	Z[:,0]=r*np.cos(theta)
+	Z[:,2]=r*np.sin(theta)
+	return Z
+
+starting_thetas=[random.uniform(0,2*m.pi) for __ in range(number_of_asteroids)]
+'''print(cylindrical_to_cartesian_transformation(asteroid_sun_distance,starting_thetas))'''
+
+#liczymy macierz obrotu od wektora mars_orbiting_speed (wikipedia)
+starting_velocities_r = [-mars_orbiting_speed*m.sin(random.uniform(0,m.pi/2)) for __ in range(number_of_asteroids)]
+starting_velocities_theta = vec_function(mars_orbiting_speed ,starting_velocities_r)
+'''print(starting_velocities_theta)'''
+
+def make_starting_array(r,theta,funct):
+	Y[:,1] = starting_velocities_r
+	Y[:,3] = starting_velocities_theta
+	return funct(r,theta,Y)
+print(make_starting_array(asteroid_sun_distance,starting_thetas,cylindrical_to_cartesian_transformation))
+print(Y[0,1])
+
+sun_sgp=4
+earth_sgp=3
+moon_sqp=2
+sun_coordinates=[0,0]
+earth_sun_distance=3
+step=0.01
+liminal_time=25.0
+radial_earth_speed=1
+def earth_coordinates(theta,funct):
+	return funct(earth_sun_distance,theta,B)
+
+B = np.zeros(shape=(int(liminal_time/step),3))
+B[:,0]= np.arange(0,liminal_time,step)
+#wspolrzedne x-owe
+'''B[:,1]= earth_sun_distance*np.cos(radial_speed*B[:,0])
+B[:,2]= earth_sun_distance*np.sin(radial_speed*B[:,0])'''
+print(B)
 
 
-sun = turtle.Turtle()
-sun.shape("circle")
-sun.color("yellow")
-sun.resizemode()
-sun.shapesize(stretch_wid=5, stretch_len=5)
-sun.penup()
-sun.speed(0)
-sun.goto(0,0)
 
-turtle
-angle=0
-sun_angle=0
-turtle
-area_width=[-900,earth.xcor()]
-area_height=[-500,500]
+'''class asteroid_data:
+	def '''
 
-def moon_xforce(x,alpha=10**3):
-    return (alpha/((asteroid.xcor()-x)**2+(asteroid.ycor()-moon.ycor())**2)**1.5)*(-asteroid.xcor()+x)
-def earth_xforce(x,beta=2*10**3):
-    return (beta/((asteroid.xcor()-x)**2+(asteroid.ycor()-earth.ycor())**2)**1.5)*(-asteroid.xcor()+x)
-def sun_xforce(x,gamma=5*10**3):
-    return (gamma/((asteroid.xcor()-x)**2+(asteroid.ycor()-sun.ycor())**2)**1.5)*(-asteroid.xcor()+x)
-def moon_yforce(y,alpha=10**3):
-    return (alpha/((asteroid.xcor()-y)**2+(asteroid.ycor()-moon.ycor())**2)**1.5)*(-asteroid.ycor()+y)
-def earth_yforce(y,beta=2*10**3):
-    return (beta/((asteroid.xcor()-y)**2+(asteroid.ycor()-earth.ycor())**2)**1.5)*(-asteroid.ycor()+y)
-def sun_yforce(y,gamma=5*10**3):
-    return (gamma/((asteroid.xcor()-y)**2+(asteroid.ycor()-sun.ycor())**2)**1.5)*(-asteroid.ycor()+y)
+file = open("dane.txt","w+")
+'''data.csv'''
+file.truncate(0)
+file.close()
 
-y=random.uniform(area_height[0], area_height[1])
-x=random.uniform(area_width[0], area_width[1])
 
-asteroid = turtle.Turtle()
-asteroid.shape("circle")
-asteroid.color("red")
-asteroid.resizemode()
-asteroid.shapesize(stretch_wid=0.3, stretch_len=0.3)
-asteroid.penup()
-asteroid.speed(0)
-asteroid.goto(x,y)
+def solvr(Y,t):
+    return[-Y[1],sun_sgp/(np.hypot(Y[0], Y[2])**3)*Y[0],-Y[3],sun_sgp/(np.hypot(Y[0], Y[2])**3)*Y[2]]
 
-moon.dangle=0.036
-earth.dangle=0.003
-asteroid.dx=0
-asteroid.dy=0
-while True:
-    asteroid.dx+= moon_xforce(moon.xcor())+earth_xforce(earth.xcor())+sun_xforce(sun.xcor())
-    asteroid.dy+= moon_yforce(moon.ycor())+earth_yforce(earth.ycor())+sun_yforce(sun.ycor())
-    asteroid.setx(asteroid.xcor()+asteroid.dx)
-    asteroid.sety(asteroid.ycor()+asteroid.dy)
-    moon.setx(earth.xcor()+radius*m.cos(angle))
-    moon.sety(earth.ycor()+radius*m.sin(angle))
-    earth.setx(sun.xcor()-sun_distance*m.cos(sun_angle))
-    earth.sety(sun.ycor()-sun_distance*m.sin(sun_angle))
-    sun_angle=sun_angle+earth.dangle
-    angle=angle+moon.dangle
+def main(i):
+    a_t = np.arange(0, liminal_time, step)
 
-wn.mainloop()
+
+    asol = integrate.odeint(solvr, [Y[i,0],Y[i,1],Y[i,2],Y[i,3]], a_t)
+    astack = np.c_[a_t, asol[:,0], asol[:, 1],asol[:,2], asol[:, 3]]
+    np.savetxt('dane.txt', astack, delimiter=',', header='t, dx,vx,dy,vy', comments='')
+
+processes = []
+i=0
+for _ in range(number_of_asteroids):
+	p= multiprocessing.Process(target=main(i))
+	p.start()
+	i+=1
+	processes.append(p)
